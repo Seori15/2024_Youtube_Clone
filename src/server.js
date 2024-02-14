2bd15e4e06dac7ae24b8d067b8def411c860220f
Seori15 <wlstn6278@gmail.com>
2024-02-15 01:14:53 +0900

    #6 GET, POST

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const logger = morgan("dev");
 app.use(logger);
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
-
+app.use(express.urlencoded({ extended: true }));
 app.use("/", globalRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
