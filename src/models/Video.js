e6a3ac438b8f50c5c94cbf72cbb08eb5319627cc
Seori15 <wlstn6278@gmail.com>
2024-02-21 23:39:57 +0900

    #6 CRUD Video

--- src/models/Video.js
+++ src/models/Video.js
@@ -11,5 +11,9 @@ const videoSchema = new mongoose.Schema({
   hashtags: [{ type: String, trim: true }],
 });
 
+videoSchema.static('formatHashtags', function(hashtags) {
+  return hashtags.split(",").map((word) => (word.startsWith("#") ? word : `#${word}`));
+})
+
 const Video = mongoose.model("Video", videoSchema);
 export default Video;
